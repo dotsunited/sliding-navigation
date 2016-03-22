bafec31575ecb9933e59658e297927c85f02ef93
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2016-03-22 12:08:08 +0100

    Add second demo with different options

--- demo/_src/index.js
+++ demo/_src/index.js
@@ -1,4 +1,11 @@
 require('./style.less');
 
 var setup = require('../../lib/setup');
+
 setup('sliding-navigation');
+
+setup('sliding-navigation', {
+    selector: '.sliding-navigation2',
+    openOnItemClick: true,
+    openButtonElement: '<span class="sliding-navigation__open-button"></span>'
+});
